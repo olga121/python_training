b541ce6bd44e8ec014aec3077ad722c5f2acb991
olga121 <igolv2003@yahoo.com>
2016-08-27 16:51:42 +0100

    тест для нового контакта тоже переведен на pytest

--- application.py
+++ application.py
@@ -53,3 +53,26 @@ class Application:
 
     def destroy(self):
         self.wd.quit()
+
+    def fill_new_contact_form(self, new_contact):
+        wd = self.wd
+        self.open_add_new_contact()
+        # fill new contact form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(new_contact.name)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(new_contact.last_name)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(new_contact.home_number)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(new_contact.email)
+        # submit the form
+        wd.find_element_by_name("submit").click()
+
+    def open_add_new_contact(self):
+        wd = self.wd
+        wd.find_element_by_link_text("add new").click()
--- test_add_contact.py
+++ test_add_contact.py
@@ -1,65 +1,17 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-import unittest
+import pytest
 from new_contact import New_contact
+from application import Application
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
 
-class test_add_contact(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
-    
-    def test_add_contact(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.loging(wd, user="admin", password="secret")
-        self.open_add_new_contact(wd)
-        self.fill_new_contact_form(wd, New_contact(name="Olga", last_name="Ivanova", home_number="84957777777", email="email@mail.ru"))
-        self.logout(wd)
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
 
-    def fill_new_contact_form(self, wd, new_contact):
-        # fill new contact form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(new_contact.name)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(new_contact.last_name)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(new_contact.home_number)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(new_contact.email)
-        # submit the form
-        wd.find_element_by_name("submit").click()
-
-    def open_add_new_contact(self, wd):
-        wd.find_element_by_link_text("add new").click()
-
-    def loging(self, wd, user, password):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(user)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/")
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == '__main__':
-    unittest.main()
+def test_add_contact(app):
+    app.login(username="admin", password="secret")
+    app.fill_new_contact_form(New_contact(name="Olga", last_name="Ivanova", home_number="84957777777", email="email@mail.ru"))
+    app.logout()
