ded4d70cf525bf760956cd51acf1a9ba7a5c54d9
olga121 <igolv2003@yahoo.com>
2016-09-12 21:32:48 +0100

    добавлены предусловия в тестах для удаления и модификации групп

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,10 +2,16 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modify_first_group(Group(name="new name"))
 
 def test_modify_group_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modify_first_group(Group(header="new header"))
 
 def test_modify_group_footer(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modify_first_group(Group(footer="new footer"))
