ab54a70f24e00f9f261810313444bf035562e866
olga121 <igolv2003@yahoo.com>
2016-09-25 14:26:13 +0100

    реализованы генераторы тестовых данных, сохраняющие данные в формате JSON

---
+++ generator/contact.py
@@ -0,0 +1,33 @@
+from model.new_contact import New_contact
+import random
+import string
+import os.path
+import jsonpickle
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+def random_string_letters(prefix, maxlen):
+    symbols = string.ascii_letters
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+def random_string_numbers(prefix, maxlen):
+    symbols = string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [New_contact(name="", last_name="", home_number="", mobile_number = "", work_number = "",
+                 secondary_number = "", email="", email2 = "", email3 = "", address = "")] + [
+    New_contact(name=random_string_letters("name", 10), last_name=random_string_letters("last_name", 20),
+                mobile_number = random_string_numbers("mobile_number", 20), work_number = random_string_numbers("work_number", 20),
+                secondary_number= random_string_numbers("secondary_number", 20), email=random_string("email", 10),
+                email2 = random_string("email2", 10), email3 = random_string("email3", 10), address = random_string("address", 20),
+                home_number=random_string_numbers("home_number", 20))
+    for i in range(5)
+]
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../data/contacts.json")
+
+with open(file, "w") as out:
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,30 +2,29 @@
 
 from model.new_contact import New_contact
 import pytest
-import random
-import string
+from data.contacts import testdata
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+#def random_string(prefix, maxlen):
+#    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+#    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-def random_string_letters(prefix, maxlen):
-    symbols = string.ascii_letters
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+#def random_string_letters(prefix, maxlen):
+#    symbols = string.ascii_letters
+#    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-def random_string_numbers(prefix, maxlen):
-    symbols = string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+#def random_string_numbers(prefix, maxlen):
+#    symbols = string.digits + string.punctuation + " "*10
+#    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-testdata = [New_contact(name="", last_name="", home_number="", mobile_number = "", work_number = "",
-                 secondary_number = "", email="", email2 = "", email3 = "", address = "")] + [
-    New_contact(name=random_string_letters("name", 10), last_name=random_string_letters("last_name", 20),
-                mobile_number = random_string_numbers("mobile_number", 20), work_number = random_string_numbers("work_number", 20),
-                secondary_number= random_string_numbers("secondary_number", 20), email=random_string("email", 10),
-                email2 = random_string("email2", 10), email3 = random_string("email3", 10), address = random_string("address", 20),
-                home_number=random_string_numbers("home_number", 20))
-    for i in range(5)
-]
+#testdata = [New_contact(name="", last_name="", home_number="", mobile_number = "", work_number = "",
+#                 secondary_number = "", email="", email2 = "", email3 = "", address = "")] + [
+#    New_contact(name=random_string_letters("name", 10), last_name=random_string_letters("last_name", 20),
+#                mobile_number = random_string_numbers("mobile_number", 20), work_number = random_string_numbers("work_number", 20),
+#                secondary_number= random_string_numbers("secondary_number", 20), email=random_string("email", 10),
+#                email2 = random_string("email2", 10), email3 = random_string("email3", 10), address = random_string("address", 20),
+#                home_number=random_string_numbers("home_number", 20))
+#    for i in range(5)
+#]
 
 @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
 def test_add_contact(app, contact):
